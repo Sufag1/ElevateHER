9eb690ab09a18fa52ac37dcc9a825a63e96b98ed
Ali <alibelloimoukhuede@gmail.com>
2025-07-20 14:48:26 +0100

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "./Navbar.css";
+import Logo from "../../assets/elevateher_logo_black.svg"
 
 const Navbar = () => {
   return (
@@ -6,19 +7,16 @@ const Navbar = () => {
       <div className="navbar-top-border"></div>
       <div className="navbar-container">
         <div className="navbar-logo">
-          <span className="logo-icon">❄</span>
-          <span className="logo-text">
-            PHLOX <strong>Business</strong>
-          </span>
+            <img src={Logo} alt="" className="logo-icon" />
         </div>
         <nav className="navbar-links">
           <a href="#" className="active">Home</a>
-          <a href="#">Blog</a>
-          <a href="#">Portfolio</a>
+          <a href="#">Courses</a>
+          <a href="#">Events</a>
           <a href="#">About Us</a>
           <a href="#">Contact Us</a>
         </nav>
-        <button className="get-started">Get Started</button>
+        <button className="get-started btn">Get Started</button>
       </div>
     </header>
   );
