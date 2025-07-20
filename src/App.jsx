a4ecc366e164f96ade6fab6ccb596a8766e6ee1c
Ali <alibelloimoukhuede@gmail.com>
2025-07-20 06:39:10 +0100

    add spacing and font variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import './App.css'
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello React + Vite + Bootstrap!</h1>
+      <h1 className='hello'>Hello React + Vite + Bootstrap!</h1>
       <button>Click Me</button>
     </div>
   );
