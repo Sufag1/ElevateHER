50e918badfc9d8cbd8cc09b54188d04d6d1f252c
Ali <alibelloimoukhuede@gmail.com>
2025-07-21 16:20:36 +0100

    update HERO SECTION

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,20 +3,22 @@ import Logo from "../../assets/elevateher_logo_black.svg"
 
 const Navbar = () => {
   return (
-    <header className="navbar">
-      <div className="navbar-top-border"></div>
-      <div className="navbar-container">
-        <div className="navbar-logo">
-            <img src={Logo} alt="" className="logo-icon" />
+    <header className="navbar-section container">
+      <div className="navbar">
+        <div className="navbar-top-border"></div>
+        <div className="navbar-container">
+          <div className="navbar-logo">
+              <img src={Logo} alt="" className="logo-icon" />
+          </div>
+          <nav className="navbar-links">
+            <a href="#" className="active">Home</a>
+            <a href="#">Courses</a>
+            <a href="#">Events</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+          </nav>
+          <button className="get-started btn">Get Started</button>
         </div>
-        <nav className="navbar-links">
-          <a href="#" className="active">Home</a>
-          <a href="#">Courses</a>
-          <a href="#">Events</a>
-          <a href="#">About Us</a>
-          <a href="#">Contact Us</a>
-        </nav>
-        <button className="get-started btn">Get Started</button>
       </div>
     </header>
   );
