26474fec1a21fc134046f337b493d1ddf7fd5a90
Ali <alibelloimoukhuede@gmail.com>
2025-07-23 08:54:48 +0100

    complete news section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/Navbar/Navbar";
 import HeroSection from "../../components/HeroSection/HeroSection";
+import BlogSection from "../../components/Blog/Blog";
 
 function Home() {
   return (
     <div>
         <Navbar />
         <HeroSection />
+        <BlogSection />
     </div>
   )
 }
