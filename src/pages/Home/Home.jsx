5a706d0eaae258d6566989ba3bb509166c086f01
Mmeso-iconic <mesougwuanyi@gmail.com>
2025-07-23 17:07:09 +0100

    Client Review 1.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/Navbar/Navbar";
 import HeroSection from "../../components/HeroSection/HeroSection";
+import ClientsReview from "../../components/Clients-Review/ClientsReview";
 
 function Home() {
   return (
     <div>
         <Navbar />
         <HeroSection />
+        <ClientsReview />
     </div>
   )
 }
