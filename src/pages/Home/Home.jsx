f003b49024d009610fd3157c4fb38e0b2d5aec72
Sufag1 <farouqabubakar99@gmail.com>
2025-07-24 13:33:02 +0100

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,18 +3,14 @@ import HeroSection from "../../components/HeroSection/HeroSection";
 import OurSolutions from "../../components/OurSolutions/Solutions";
 
 import BlogSection from "../../components/Blog/Blog";
- dev
 
 function Home() {
   return (
     <div>
         <Navbar />
         <HeroSection />
- our-solutions-and-features
         <OurSolutions />
-
         <BlogSection />
- dev
     </div>
   )
 }
