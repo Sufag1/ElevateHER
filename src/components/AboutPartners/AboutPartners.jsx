a06bc6079735f757b100068c65ba2fa7947f084b
Ali <alibelloimoukhuede@gmail.com>
2025-12-17 18:07:55 +0300

    started content upload 6

--- src/components/AboutPartners/AboutPartners.jsx
+++ src/components/AboutPartners/AboutPartners.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./AboutPartners.css"
-import Partner1 from "../../assets/Review-Assets/Partner1.svg"
-import Partner2 from "../../assets/Review-Assets/Partner2.svg"
-import Partner3 from "../../assets/Review-Assets/Partner3.svg"
-import Partner4 from "../../assets/Review-Assets/Partner4.svg"
-import Partner5 from "../../assets/Review-Assets/Partner5.svg"
+import Partner1 from "../../assets/paf-logo.webp"
+import Partner2 from "../../assets/AK&A-Logo.webp"
+import Partner3 from "../../assets/giz-logo.svg"
+import Partner4 from "../../assets/remoting.webp"
+import Partner5 from "../../assets/tvet.webp"
+import Partner6 from "../../assets/logo-sikili.webp"
 
 
 const AboutPartners = () => {
@@ -22,12 +23,14 @@ const AboutPartners = () => {
                         <img src={Partner3} alt="Partner 3" />
                         <img src={Partner4} alt="Partner 4" />
                         <img src={Partner5} alt="Partner 5" />
+                        <img src={Partner6} alt="Partner 6" />
                         {/* repeating logos to help with infinite effect */}
                         <img src={Partner1} alt="Partner 1" />
                         <img src={Partner2} alt="Partner 2" />
                         <img src={Partner3} alt="Partner 3" />
                         <img src={Partner4} alt="Partner 4" />
                         <img src={Partner5} alt="Partner 5" />
+                        <img src={Partner6} alt="Partner 6" />
                     </div>
                 </div>
             </div>
